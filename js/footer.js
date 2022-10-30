87e1c9b552903ae3de3490a19f252446ddc90f6c
hiyu <hiyudon0524@gmail.com>
2022-10-30 19:46:55 +0900

    w

---
+++ js/footer.js
@@ -0,0 +1,18 @@
+document.querySelector('#footer').innerHTML = `
+    <main id="main1">
+      <div class="tos">
+        <p>
+          利用規約
+        </p>
+      </div>
+      <div class="pp">
+        <p>
+          プライバシーポリシー
+        </p>
+      </div>
+      <div class="gc">
+        <p>
+          GraphicsCreate
+        </p>
+      </div>
+    </main>`;
