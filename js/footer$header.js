80f0f726a9a129c1ba6ec96b8bf256aef11822ec
EARTH <92440881+earth-web@users.noreply.github.com>
2022-11-03 18:37:51 +0900

    s

--- js/footer$header.js
+++ js/footer$header.js
@@ -17,8 +17,8 @@ document.querySelector('#footer').innerHTML = `
       |
       </div>
       <div class="gc">
-        <a href="https://graphicscreate.github.io/GraphicsCreate/">
-          GraphicsCreate
+        <a href="#">
+          お問い合わせ
         </a>
       </div>
     </div>
@@ -29,7 +29,29 @@ document.querySelector('#footer').innerHTML = `
       <div id="year">
       </div>
     </div>
-`;
+  `;
+document.querySelector('#rheader').innerHTML = `
+  <div class="headerda">
+    <a href="https://earth-web.github.io/">
+      <img src="img/icon 01.png"></img>
+    </a>
+  </div>
+  <div class="header-link">
+    <div class="header-link-f">
+      <a href="blog/bloghome.html">BLOG</a>
+    </div>
+    <div class="header-link-s">
+      <a href="">
+
+      </a>
+    </div>
+    <div class="header-link-t">
+      <a href="">
+
+      </a>
+    </div>
+  </div>
+  `;
   date = new Date();
   year = date.getFullYear();
   document.getElementById("year").innerHTML = year;
